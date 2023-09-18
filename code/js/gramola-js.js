604642b994d30d83771b183c97ae5d956d997e36
Adria Garcia <2704adria@gmail.com>
2023-09-18 19:07:09 +0200

    Gramola 3 - HTML, CSS, JS, JSON

---
+++ code/js/gramola-js.js
@@ -0,0 +1,15 @@
+function aleatori(){
+    alert("aleatori")
+}
+
+function enrere(){
+    alert("enrere")
+}
+
+function play(){
+    alert("pausa")
+}
+
+function avançar(){
+    alert("avançar")
+}
